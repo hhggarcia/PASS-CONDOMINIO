7006b05b983a6328db72f2911f3705a2ef37d48e
hhggarcia <hgarcia@password.com.ve>
2022-09-20 16:50:19 -0400

    creando otra rama

---
+++ Prueba/Migrations/PruebaMigrations/20220920201309_CodigosCuentasCrear.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prueba.Migrations.PruebaMigrations
+{
+    public partial class CodigosCuentasCrear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
