aa707951c691443291bca576b9392854c78bd641
hhggarcia <hgarcia@password.com.ve>
2022-09-26 12:45:12 -0400

    Registro pago - en construccion

---
+++ Prueba/Models/RegistroPagoVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Prueba.Models
+{
+    public class RegistroPagoVM: PagoEmitido
+    {
+        public string? Descripcion { get; set; }
+        public string? Concepto { get; set; }
+        public int IdCodigoCuenta { get; set; }
+        public int TipoOperacion { get; set; }
+        public IList<SelectListItem>? SubCuentasGastos { get; set; }
+        public int IdSubcuenta { get; set; }
+
+    }
+}
