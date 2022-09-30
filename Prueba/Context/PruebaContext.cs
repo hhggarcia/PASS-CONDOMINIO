fbe2058d72e79f622e4cd46eda44ffe318e6fb35
Angel Becerra <angelfabian.becerra@gmail.com>
2022-09-30 12:17:34 -0400

    relación de gastos, registrar pagos

--- Prueba/Context/PruebaContext.cs
+++ Prueba/Context/PruebaContext.cs
@@ -68,7 +68,7 @@ namespace Prueba.Context
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server= DESKTOP-NF5DL1U\\SQLEXPRESS01; Database= Prueba;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server= DESKTOP-LSMA140\\SQLEXPRESS; Database= Prueba;Trusted_Connection=True;");
             }
         }
 
