a6ba50cb0cc2ca450a11d8e38a5d2c52309fca99
hhggarcia <hgarcia@password.com.ve>
2022-12-15 14:13:01 -0400

    areas

--- Prueba/wwwroot/js/demo/chart-area-demo.js
+++ Prueba/wwwroot/js/demo/chart-area-demo.js
@@ -29,10 +29,23 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 
 // Area Chart Example
 var ctx = document.getElementById("myAreaChart");
+//ingresos
+var ingresoEnero = $("#ingreso1").val();
+var ingresoFebrero = $("#ingreso2").val();
+var ingresoMarzo = $("#ingreso3").val();
+var ingresoAbril = $("#ingreso4").val();
+var ingresoMayo = $("#ingreso5").val();
+var ingresoJunio = $("#ingreso6").val();
+var ingresoJulio = $("#ingreso7").val();
+var ingresoAgosto = $("#ingreso8").val();
+var ingresoSeptiembre = $("#ingreso9").val();
+var ingresoOctubre = $("#ingreso10").val();
+var ingresoNoviembre = $("#ingreso11").val();
+var ingresoDiciembre = $("#ingreso12").val();
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ag", "Sep", "Oct", "Nov", "Dic"],
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
@@ -46,7 +59,109 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 0],
+        data: [ingresoEnero, ingresoFebrero, ingresoMarzo, ingresoAbril, ingresoMayo, ingresoJunio, ingresoJulio, ingresoAgosto, ingresoSeptiembre, ingresoOctubre, ingresoNoviembre, ingresoDiciembre],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return '$' + number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        }
+      }
+    }
+  }
+});
+// Area Chart Egresos
+var ctx = document.getElementById("myAreaChartEgresos");
+//ingresos
+var eEnero = $("#egreso1").val();
+var eFebrero = $("#egreso2").val();
+var eMarzo = $("#egreso3").val();
+var eAbril = $("#egreso4").val();
+var eMayo = $("#egreso5").val();
+var eJunio = $("#egreso6").val();
+var eJulio = $("#egreso7").val();
+var eAgosto = $("#egreso8").val();
+var eSeptiembre = $("#egreso9").val();
+var eOctubre = $("#egreso10").val();
+var eNoviembre = $("#egreso11").val();
+var eDiciembre = $("#egreso12").val();
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ag", "Sep", "Oct", "Nov", "Dic"],
+    datasets: [{
+      label: "Earnings",
+      lineTension: 0.3,
+      backgroundColor: "rgba(78, 115, 223, 0.05)",
+      borderColor: "rgba(78, 115, 223, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+        data: [eEnero, eFebrero, eMarzo, eAbril, eMayo, eJunio, eJulio, eAgosto, eSeptiembre, eOctubre, eNoviembre, eDiciembre],
     }],
   },
   options: {
