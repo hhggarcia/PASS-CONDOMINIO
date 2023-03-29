476500d55b262fc067dfbb09c9a56bb3f734ea9b
DesaMiguel <127340639+DesaMiguel@users.noreply.github.com>
2023-03-29 11:43:15 -0400

    Update PDFServices.cs

--- Prueba/Services/PDFServices.cs
+++ Prueba/Services/PDFServices.cs
@@ -45,52 +45,59 @@ namespace Prueba.Services
 
                         row.RelativeItem().Column(col =>
                         {
-                            col.Item().AlignCenter().Text("Codigo Estudiante SAC").Bold().FontSize(14);
-                            col.Item().AlignCenter().Text("Jr. Las mercedes N378 - Lima").FontSize(9);
-                            col.Item().AlignCenter().Text("987 987 123 / 02 213232").FontSize(9);
-                            col.Item().AlignCenter().Text("codigo@example.com").FontSize(9);
+                            col.Item().AlignCenter().Text("Junta de condominio Residencias Parque Humbold").Bold().FontSize(14);
 
                         });
 
                         row.RelativeItem().Column(col =>
                         {
-                            col.Item().Border(1).BorderColor("#257272")
-                            .AlignCenter().Text("RUC 21312312312");
+                            string date = DateTime.UtcNow.ToString("dd-MM-yyyy");
+                            col.Item().Column(col =>
+                            {
+                                col.Item().Border(1).BorderColor("#257272").
+                                AlignCenter().Text(date);
 
-                            col.Item().Background("#257272").Border(1)
-                            .BorderColor("#257272").AlignCenter()
-                            .Text("Boleta de venta").FontColor("#fff");
+                                col.Item().Background("#257272").Border(1)
+                                .BorderColor("#257272").AlignCenter()
+                                .Text("Alicuotas").FontColor("#fff");
 
-                            col.Item().Border(1).BorderColor("#257272").
-                            AlignCenter().Text("B0001 - 234");
-
-
+                                col.Item().Border(1).BorderColor("#257272").
+                                AlignCenter().Text("Costo de alicuota");
+                            });
                         });
+
                     });
 
                     page.Content().PaddingVertical(10).Column(col1 =>
                     {
                         col1.Item().Column(col2 =>
                         {
-                            col2.Item().Text("Datos del cliente").Underline().Bold();
 
                             col2.Item().Text(txt =>
                             {
-                                txt.Span("Nombre: ").SemiBold().FontSize(10);
+                                txt.Span("Propietario: ").SemiBold().FontSize(10);
                                 txt.Span("Mario mendoza").FontSize(10);
                             });
 
                             col2.Item().Text(txt =>
                             {
-                                txt.Span("DNI: ").SemiBold().FontSize(10);
-                                txt.Span("978978979").FontSize(10);
+                                txt.Span("Torre: ").SemiBold().FontSize(10);
+                                txt.Span("A").FontSize(10);
                             });
 
                             col2.Item().Text(txt =>
                             {
-                                txt.Span("Direccion: ").SemiBold().FontSize(10);
-                                txt.Span("av. miraflores 123").FontSize(10);
+                                txt.Span("Piso: ").SemiBold().FontSize(10);
+                                txt.Span("2").FontSize(10);
                             });
+
+                            col2.Item().Text(txt =>
+                            {
+                                txt.Span("Apartamento: ").SemiBold().FontSize(10);
+                                txt.Span("51-A").FontSize(10);
+                            });
+
+
                         });
 
                         col1.Item().LineHorizontal(0.5f);
@@ -99,57 +106,87 @@ namespace Prueba.Services
                         {
                             tabla.ColumnsDefinition(columns =>
                             {
-                                columns.RelativeColumn(3);
-                                columns.RelativeColumn();
-                                columns.RelativeColumn();
-                                columns.RelativeColumn();
+                                columns.RelativeColumn(2);
+                                columns.RelativeColumn(2);
+                                columns.RelativeColumn(10);
+                                columns.RelativeColumn(2);
+                                columns.RelativeColumn(2);
+                                columns.RelativeColumn(2);
 
                             });
 
                             tabla.Header(header =>
                             {
-                                header.Cell().Background("#257272")
-                                .Padding(2).Text("Producto").FontColor("#fff");
+                                header.Cell().Background("#257272").Text("Fecha").FontColor("#fff").FontSize(10);
 
-                                header.Cell().Background("#257272")
-                               .Padding(2).Text("Precio Unit").FontColor("#fff");
+                                header.Cell().Background("#257272").Text("Tipo de Gasto").FontColor("#fff").FontSize(10);
 
-                                header.Cell().Background("#257272")
-                               .Padding(2).Text("Cantidad").FontColor("#fff");
+                                header.Cell().Background("#257272").AlignCenter().Text("Descripcion").FontColor("#fff").FontSize(10);
 
-                                header.Cell().Background("#257272")
-                               .Padding(2).Text("Total").FontColor("#fff");
+                                header.Cell().Background("#257272").AlignRight().Text("Monto Bs.").FontColor("#fff").FontSize(10);
+
+                                header.Cell().Background("#257272").AlignRight().Text("Alicuota").FontColor("#fff").FontSize(10);
+
+                                header.Cell().Background("#257272").AlignRight().Text("Mto a \r\nPagar Bs.").FontColor("#fff").FontSize(10);
                             });
 
-                            foreach (var item in Enumerable.Range(1, 45))
+                            foreach (var item in Enumerable.Range(1, 6))
                             {
-                                var cantidad = Placeholders.Random.Next(1, 10);
-                                var precio = Placeholders.Random.Next(5, 15);
-                                var total = cantidad * precio;
+                                //Fecha actual
+                                string date = DateTime.UtcNow.ToString("dd-MM-yyyy");
+                                //precio de mes deudor
+                                var precio = Placeholders.Random.Next(5);
+                                //Total de la deuda
+                                var total = precio;
 
-                                tabla.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9")
-                                .Padding(2).Text(Placeholders.Label()).FontSize(10);
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).Text(date).FontSize(6);
+                                //Tipo de gasto
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).Text("Ordinario").FontSize(6);
 
-                                tabla.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9")
-                                .Padding(2).Text(cantidad.ToString()).FontSize(10);
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).Text("Descricion").FontSize(6);
 
-                                tabla.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9")
-                                .Padding(2).Text($"S/. {precio}").FontSize(10);
+                                //LG es para Leyenda de gasto
 
-                                tabla.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9")
-                                .Padding(2).AlignRight().Text($"S/. {total}").FontSize(10);
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).AlignRight().Text("LG").FontSize(6);
+
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).AlignRight().Text("Alicuota").FontSize(6);
+
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).AlignRight().Text("LG").FontSize(6);
+
                             }
+                            foreach (var item in Enumerable.Range(1, 6))
+                            {
+
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).Text("").FontSize(6);
+                                //Tipo de gasto
+                                tabla.Cell().Border(1).BorderBottom(0.5f).BorderColor("#D9D9D9")
+                                .Padding(2).Text("").FontSize(6);
+
+                            }
+
+                            col1.Item().AlignRight().Text("Total: ").FontSize(12);
+
+                            col1.Item().Padding(10)
+                            .Column(column =>
+                            {
+                                column.Item().Background(Transparent).AlignCenter().Text("Elaborado por Junta de Condominio Res Parque Humboldt\r\nRealizar los pagos por Transferencias a la cuenta\r\nBANCO MERCANTIL\r\nJunta de Condominio Residencias Parque Humboldt\r\nRif J-30720421-4\r\nCta. Cte. 0105-0021-46802103-9647\r\nEnviar al correo : adm.parque.humboldt@gmail.com\r\nFavor indicar número de Apartamento ").FontSize(8);
+                            });
 
                         });
 
-                        col1.Item().AlignRight().Text("Total: 1500").FontSize(12);
-
                         if (1 == 1)
-                            col1.Item().Background(Colors.Grey.Lighten3).Padding(10)
+                            col1.Item().Background(Transparent).Padding(10)
                             .Column(column =>
                             {
-                                column.Item().Text("Comentarios").FontSize(14);
-                                column.Item().Text(Placeholders.LoremIpsum());
+                                column.Item().Text("Observaciones").FontSize(14);
+
                                 column.Spacing(5);
                             });
 
