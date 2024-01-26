66ae90161da4120f125e4d0e1d5dccabfb4f8d79
hhggarcia <hgarcia@password.com.ve>
2024-01-26 16:06:37 -0400

    subcuentas cristian

---
+++ Prueba/ViewModels/CuentaGlobalSubCuentasVM.cs
@@ -0,0 +1,11 @@
+﻿using Prueba.Models;
+
+namespace Prueba.ViewModels
+{
+    public class CuentaGlobalSubCuentasVM
+    {
+        public SubCuenta? SubCuentas { get; set; } = new SubCuenta();
+        public decimal Saldo { get; set; }
+        public decimal SaldoInicial { get; set; }
+    }
+}
