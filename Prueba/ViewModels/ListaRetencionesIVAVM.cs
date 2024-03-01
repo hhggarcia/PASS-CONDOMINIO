35b8bf3c89f7641e3853851bb1013031292285ef
hhggarcia <hgarcia@password.com.ve>
2024-03-01 10:08:01 -0400

    Ajustes pdf

---
+++ Prueba/ViewModels/ListaRetencionesIVAVM.cs
@@ -0,0 +1,15 @@
+﻿namespace Prueba.ViewModels
+{
+    public class ListaRetencionesIVAVM
+    {
+        public string Comprobante { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Proveedor { get; set; }
+        public string NumFactura { get; set; }
+        public string Concepto { get; set; }
+        public decimal TotalFactura { get; set; }
+        public decimal Iva { get; set; }
+        public decimal Retencones { get; set; }
+        public decimal Retenido { get; set; }
+    }
+}
