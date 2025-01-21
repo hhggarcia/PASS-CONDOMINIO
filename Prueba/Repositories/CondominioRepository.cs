a76d9ee7417e327a707429abed3f0abd3e956e90
hhggarcia <hgarcia@password.com.ve>
2025-01-21 16:50:18 -0400

    add multiple propiedades to one user

--- Prueba/Repositories/CondominioRepository.cs
+++ Prueba/Repositories/CondominioRepository.cs
@@ -17,16 +17,6 @@ namespace Prueba.Repositories
             _context = context;
         }
 
-        /// <summary>
-        /// buscar todos los propietarios de una condominio
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        //public async Task<IList<AspNetUser>> GetUsersCondominio(int id)
-        //{
-
-        //}
-
         public async Task<IList<Propiedad>> GetPropiedadesCondominio(int id) => await _context.Propiedads.Include(c => c.IdUsuarioNavigation)
                 .Where(c => c.IdCondominio == id)
                 .OrderBy(c => c.Codigo)
